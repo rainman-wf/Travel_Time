2e1e3588cc7a64cb49f2bc6f2ccd9e7ed2e0ad84
rainman-wf <denviva@gmail.com>
2023-06-09 19:57:37 +0300

    fragments & navigation

---
+++ ui/src/main/java/ru/netology/ui/TravelDetailsFragment.kt
@@ -0,0 +1,6 @@
+package ru.netology.ui
+
+import androidx.fragment.app.Fragment
+
+class TravelDetailsFragment : Fragment(R.layout.fragment_travel_details) {
+}
---
+++ ui/src/main/java/ru/netology/ui/TravelListFragment.kt
@@ -0,0 +1,6 @@
+package ru.netology.ui
+
+import androidx.fragment.app.Fragment
+
+class TravelListFragment : Fragment(R.layout.fragment_travel_list) {
+}
